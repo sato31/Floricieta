55b07840aa6ea0f67ea83663c3b669eeb5d6c1ac
Oscar <sanchez.torres.oscar.a@gmail.com>
2024-09-20 23:46:32 -0600

    Añadida maqueta HTML y funciones básicas de render en js

--- main.js
+++ main.js
@@ -7,10 +7,48 @@ while (numero !== 9401 && numero !== 3005) {
 }
 
 
+const renderCard = () => {
+    const card = document.querySelector("#card")    
+    card.innerHTML += `
+    <h1>Nocierto 😂</h1>
+    <h1>Acertaste!!!</h1>
 
-if (numero === 9401) {
-    alert("9401")
+    
+    `
 }
-else if (numero = 3005){
-    alert("3005")
+
+const renderSecretCard = () => {
+    const secretCard = document.querySelector("#secret-card")    
+    card.innerHTML += `
+    <h1>Tarjeta Secreta</h1>
+    `
 }
+
+    if (numero === 9401) {
+        alert(`ERROR \nNúmero incorrecto 🙁 \n Bueno no importa 😕, aprieta Aceptar`)
+        renderCard()
+    }
+    else if (numero === 3005){
+        alert("3005")
+        renderSecretCard()
+    }
+
+// const renderCards = () => {
+//     const card = document.querySelector("#card")
+//     const secretCard = document.querySelector("#secret-card")
+//     card.innerHTML = "";
+//     if (numero === 9401) {
+//         alert(`ERROR \nNúmero incorrecto 🙁 \n Bueno no importa 😕, aprieta Aceptar`)
+//         card.innerHTML += `
+//             <h1>Nocierto 😂</h1>
+//             <h1>Acertaste!!!</h1>
+//             `
+//     }
+//     else if (numero === 3005){
+//         alert("3005")
+//         secretCard.innerHTML += `
+//             <h1>Tarjeta Secreta</h1>
+//             `
+//     }
+
+// }
